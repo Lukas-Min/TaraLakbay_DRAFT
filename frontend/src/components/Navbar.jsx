bc5038686aa576330e2fdc74a9b6c820bc95864e
Lukas <132744951+Lukas-Min@users.noreply.github.com>
2024-05-23 13:09:45 +0800

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,17 +1,34 @@
 import { FaSearch } from 'react-icons/fa'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <header className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-        <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-          <span className='text-slate-500'>Tara</span>
-          <span className='text-slate-700'>Lakbay</span>
-        </h1>
+        <Link to='/'>
+          <h1 className='font-bold text-sm sm:text-xl flex flex-nowrap'>
+            <span className='text-slate-500'>Tara</span>
+            <span className='text-slate-700'>Lakbay</span>
+          </h1>
+        </Link>
         <form className='bg-slate-100 p-2 rounded-lg flex items-center' action="">
-          <input type='text' placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64' />
+          <input type='text' placeholder='Search...' className='bg-transparent focus:outline-none w-36 sm:w-64' />
           <FaSearch className='text-slate-600 cursor-pointer'></FaSearch>
         </form>
+        <ul className='flex gap-4'>
+          <Link to='/'>
+            <li className='hidden text-nowrap cursor-pointer sm:inline text-slate-700 hover:underline'>Home</li>
+          </Link>
+          <Link to='/about'>
+            <li className='hidden text-nowrap cursor-pointer sm:inline text-slate-700 hover:underline'>About</li>
+          </Link>
+          <Link to='/sign-in'>
+            <li className='hidden text-nowrap cursor-pointer sm:inline text-slate-700 hover:underline'>Sign In</li>
+          </Link>
+          <Link to='/sign-up'>
+            <li className='hidden text-nowrap cursor-pointer sm:inline text-slate-700 hover:underline'>Sign Up</li>
+          </Link>
+        </ul>
       </div>
     </header>
   )
