40c450a9e9b2a4aacc887929486a8a817ead9915
Lukas <132744951+Lukas-Min@users.noreply.github.com>
2024-05-23 19:09:18 +0800

    test

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 //* Database configuration
 dotenv.config();
@@ -21,4 +22,8 @@ app.listen(PORT, () => {
   console.log(`Sever is listening on ${PORT}`);
 });
 
-app.use("/backend/user/", userRouter);
+app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+app.use("/backend/user", userRouter);
+app.use("/backend/auth", authRouter);
