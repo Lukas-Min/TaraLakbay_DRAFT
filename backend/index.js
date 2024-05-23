b87ed8f4bba7c1438016ecb174589f523677dfa2
Lukas <132744951+Lukas-Min@users.noreply.github.com>
2024-05-23 15:00:14 +0800

    Create a test API route

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+
+//* Database configuration
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -16,4 +19,6 @@ const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Sever is listening on ${PORT}`);
-})
+});
+
+app.use("/backend/user/", userRouter);
